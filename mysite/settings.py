8877663cd1bec437d0277c73c576e60b3cabff8d
Nitin Shirsat <nitin.shirsat@avalara.com>
2015-12-24 10:44:39 -0500

    added databases settings to connect to postgres db mysite_db

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,13 +74,29 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+"""
+# default database setting
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '127.0.0.1',
+        'NAME': 'mysite_db',
+        'PORT': '5432',
+        'USER': 'pyuser',
+        'PASSWORD': 'mysite2016',
+        'TEST': {
+            'NAME': 'mysite_db_test',
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
