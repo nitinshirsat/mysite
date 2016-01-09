215af9c90c68fe822fbe2b243a5319b23fa7a3a9
Nitin Shirsat <nitin.shirsat@avalara.com>
2016-01-08 19:29:24 -0500

    added question and choice to admin

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,9 @@ class Question(models.Model):
         return self.question_text
 
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -16,3 +19,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
