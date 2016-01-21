3928cf4d3632c87603c5c2c412b491e45ed12909
Nitin Shirsat <nitin.shirsat@avalara.com>
2016-01-20 20:13:30 -0500

    added result

--- polls/views.py
+++ polls/views.py
@@ -20,12 +20,13 @@ def about_us(request):
 
 def detail(request, question_id):
     question = get_object_or_404 (Question, pk=question_id )
-    context = {'question': question,}
+    context = {'myTemplateParameter': question,}
     return render(request, 'polls/details.html', context)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404 (Question, pk=question_id )
+    context = {'question': question, }
+    return render(request, 'polls/result.html',context)
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
